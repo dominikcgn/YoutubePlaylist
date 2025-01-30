4a4db91453726c675b481d2350ade73122990ff7
dominikcgn <83615806+dominikcgn@users.noreply.github.com>
2025-01-30 22:59:05 +0100

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -8,47 +8,39 @@ banner = r'''
 #     |  __/| | | (_) | |  __/ (__| |_| |  | | (_) | (_) \__ \  __/     #
 #     |_|   |_|  \___// |\___|\___|\__|_|  |_|\___/ \___/|___/\___|     #
 #                   |__/                                                #
-#                                  >> https://github.com/benmoose39     #
 #########################################################################
 '''
 
 import requests
 import os
 import sys
-
-windows = False
-if 'win' in sys.platform:
-    windows = True
+import re
+import yt_dlp
 
 def grab(url):
-    response = requests.get(url, timeout=15).text
-    if '.m3u8' not in response:
-        #response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
-                return
-            #os.system(f'wget {url} -O temp.txt')
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
-                return
-    end = response.find('.m3u8') + 5
-    tuner = 100
-    while True:
-        if 'https://' in response[end-tuner : end]:
-            link = response[end-tuner : end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
-            break
-        else:
-            tuner += 5
-    print(f"{link[start : end]}")
+    try:
+        ydl_opts = {
+            'quiet': True,
+            'no_warnings': True,
+            'format': 'best',
+            'simulate': True,
+            'skip_download': True
+        }
+        
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            formats = info.get('formats', [])
+            for f in formats:
+                if f.get('url') and 'm3u8' in f.get('url', '').lower():
+                    print(f['url'])
+                    return
+                    
+    except Exception as e:
+        print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
 
 print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
 print(banner)
-#s = requests.Session()
+
 with open('../youtube_channel_info.txt') as f:
     for line in f:
         line = line.strip()
@@ -60,7 +52,3 @@ with open('../youtube_channel_info.txt') as f:
             url = parts[1].strip()
             print(f'\n#EXTINF:-1 group-title="Thailand", {ch_name}')
             grab(url)
-            
-if 'temp.txt' in os.listdir():
-    #os.system('rm temp.txt')
-    os.system('rm watch*')
